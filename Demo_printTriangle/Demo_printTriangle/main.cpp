e5ecdbe1550aa7fdf1c7ec4d667f9a64e92e140b
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-07 12:29:19 +1030

    HelloWorld in Xcode.

---
+++ Demo_printTriangle/Demo_printTriangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Demo_printTriangle
+//
+//  Created by QIAN CHEN on 7/2/20.
+//  Copyright © 2020 QIAN CHEN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by QIAN CHEN on 7/2/20.
+//  Copyright © 2020 QIAN CHEN. All rights reserved.
+//
+
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
